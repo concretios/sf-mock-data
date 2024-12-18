f8c859fc53c4d0635a7211bef1bbc93156832a32
Divy Muni <Divy.Muni@aexp.com>
2024-12-18 14:25:15 +0530

    ci: npm init

--- src/data_types/text/fields.ts
+++ src/data_types/text/fields.ts
@@ -1,18 +1,19 @@
-export default TextFields {
+const TextFields: any = {
 
     "first name": {
         values: ["Divy", "Rahul", "Akshat", "Karan"],
-        generate: () => {parseInt((Math.random() * 2000) + 1)}
+        generate: () => { return this.values[Math.round((Math.random() * 2000) + 1)]}
     },
 
     "last name": {
-        values: []
+        values: [],
+        generate: () => { return this.values[Math.round((Math.random() * 2000) + 1)]}
     },
 
     "default":{
         //provide 3k values with random words.
-        values: []
-        generate: () => { parseInt((Math.random() * 3000) + 1) }
+        values: [],
+        generate: () => { return this.values[Math.round((Math.random() * 3000) + 1)]}
     }
 };
 
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as text from "../src/data_types/text/generate"
+
+export default {
+    text
+}
+
+import {main} from "./modules"
